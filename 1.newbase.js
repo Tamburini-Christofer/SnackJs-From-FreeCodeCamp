abee47928e93b22b2f8c0098aa615b391705a0e9
Tamburini-Christofer <christofer.tamburini92@gmail.com>
2026-02-07 12:47:18 +0100

    NewBase per prossimi snack

---
+++ 1.newbase.js
@@ -0,0 +1,44 @@
+//todo Consegna dell'esercizio
+    // Consegna dell'esercizio
+//todo
+
+//! Requisiti da rispettare
+    // Requisiti da rispettare
+//!
+
+//* Tempo di esecuzione:
+    // Tempo impiegato 
+//*
+
+//*Codice scritto senza utilizzo di ChatGpt
+    // Codice scritto senza l'utilizzo di nessuna Ia
+//*
+
+//* Codice scritto da ChatGpt
+    // Codice scritto con l'ia, alternativa al mio o di completamento. 
+    // Se il tempo di esecuzione è stato rispettato, questo diventa un metodo alternativo di scrittura
+//*
+
+//* Valutazione al codice proposta da ChatGpt
+    // Valutazione numerica per futura media
+//*
+
+//* Pro e contro del mio codice
+    // Pro e contro e dove migliorare
+//*
+
+//! Altre considerazioni e nota bene
+
+//todo Esecuzione degli esercizi
+
+//? Creazione del file contenente "Numero snack"."Nome snack"-"Giorno del mese".js
+//? Comprensione della consegna ed eventuale copia in google translate. 
+//? 2 minuti massimo per analizzare la consegna e fare un pensiero prelimianre. 
+//? Inizio timer impostato ad un massimale di 30 minuti. Oltre questa soglia, l'esercizio non sarà completato
+//? Consultazione con ChatGpt per miglioramenti e sistemazioni
+//? Push finale
+
+//! Tre volte a settimana, riguardare lo snack e rivedere possibili soluzioni.
+//! Una volta a settimana, riguardare l'andamento mensile e sistemare le lacune.
+
+//! NON DEPRIMERSI 
