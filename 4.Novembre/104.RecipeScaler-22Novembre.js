4bf7be1f4d87ea78e6829b3c55d649a8515c34f7
Tamburini-Christofer <christofer.tamburini92@gmail.com>
2026-02-10 11:03:59 +0100

    Snack #104 RecipeScaler (da capire)

---
+++ 4.Novembre/104.RecipeScaler-22Novembre.js
@@ -0,0 +1,42 @@
+//todo Consegna dell'esercizio
+// Dato un array di ingredienti per una ricetta e un numero per scalare la ricetta, restituisci un array con le quantità scalate di conseguenza.
+//todo
+//! Requisiti da rispettare
+// Ogni elemento nell'array dato sarà una stringa nel formato: "quantità unità ingrediente". Ad esempio "2 C Farina".
+// Scala la quantità in base al numero dato. Non includere zeri finali e non convertire alcuna unità.
+// Restituisci gli elementi scalati nello stesso ordine in cui sono stati forniti.
+//!
+
+//* Tempo di esecuzione:
+    // Tempo impiegato 
+//*
+
+//*Codice scritto senza utilizzo di ChatGpt
+    // Codice scritto senza l'utilizzo di nessuna Ia
+//*
+
+//* Codice scritto da ChatGpt
+    // Codice scritto con l'ia, alternativa al mio o di completamento. 
+    // Se il tempo di esecuzione è stato rispettato, questo diventa un metodo alternativo di scrittura
+//*
+
+//* Valutazione al codice proposta da ChatGpt
+    // Valutazione numerica per futura media
+//*
+
+//* Pro e contro del mio codice
+    // Pro e contro e dove migliorare
+//*
+//! Altre considerazioni e nota bene
+//todo Esecuzione degli esercizi
+//? Creazione del file contenente "Numero snack"."Nome snack"-"Giorno del mese".js
+//? Comprensione della consegna ed eventuale copia in google translate. 
+//? 2 minuti massimo per analizzare la consegna e fare un pensiero prelimianre. 
+//? Inizio timer impostato ad un massimale di 30 minuti. Oltre questa soglia, l'esercizio non sarà completato
+//? Consultazione con ChatGpt per miglioramenti e sistemazioni
+//? Push finale
+//! Tre volte a settimana, riguardare lo snack e rivedere possibili soluzioni.
+//! Una volta a settimana, riguardare l'andamento mensile e sistemare le lacune.
+//! NON DEPRIMERSI 
+//prompt completo Chat 
+// Da ora in poi farò degli snack. Ti darà una consegna e tu dovrai analizzare la consegna, analizzare il mio codice, darmi una valutazione da 1 a 10 e dirmi i pro e i contro, poi il codice corretto
