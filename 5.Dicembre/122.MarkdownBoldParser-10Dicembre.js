f030be8da2e3297d6e5bd38f68b367a45a46f721
Tamburini-Christofer <christofer.tamburini92@gmail.com>
2026-02-14 12:53:54 +0100

    Snack #122 (non iniziato)

---
+++ 5.Dicembre/122.MarkdownBoldParser-10Dicembre.js
@@ -0,0 +1,43 @@
+//todo Consegna dell'esercizio
+// Data una stringa che potrebbe includere del testo in grassetto in Markdown, restituisci la stringa HTML equivalente.
+//todo
+//! Requisiti da rispettare
+// Il testo in grassetto in Markdown è qualsiasi testo che inizia e termina con due asterischi (**) o due caratteri di sottolineatura (__).
+// Non possono esserci spazi tra il testo e gli asterischi o i caratteri di sottolineatura, ma possono esserci spazi nel testo stesso.
+// Converti tutte le occorrenze in grassetto in tag HTML b e restituisci la stringa.
+// Ad esempio, dato "**Questo è in grassetto**", restituisci "<b>Questo è in grassetto</b>".
+//!
+
+//* Tempo di esecuzione:
+    // Tempo impiegato 
+//*
+
+//*Codice scritto senza utilizzo di ChatGpt
+    // Codice scritto senza l'utilizzo di nessuna Ia
+//*
+
+//* Codice scritto da ChatGpt
+    // Codice scritto con l'ia, alternativa al mio o di completamento. 
+    // Se il tempo di esecuzione è stato rispettato, questo diventa un metodo alternativo di scrittura
+//*
+
+//* Valutazione al codice proposta da ChatGpt
+    // Valutazione numerica per futura media
+//*
+
+//* Pro e contro del mio codice
+    // Pro e contro e dove migliorare
+//*
+//! Altre considerazioni e nota bene
+//todo Esecuzione degli esercizi
+//? Creazione del file contenente "Numero snack"."Nome snack"-"Giorno del mese".js
+//? Comprensione della consegna ed eventuale copia in google translate. 
+//? 2 minuti massimo per analizzare la consegna e fare un pensiero prelimianre. 
+//? Inizio timer impostato ad un massimale di 30 minuti. Oltre questa soglia, l'esercizio non sarà completato
+//? Consultazione con ChatGpt per miglioramenti e sistemazioni
+//? Push finale
+//! Tre volte a settimana, riguardare lo snack e rivedere possibili soluzioni.
+//! Una volta a settimana, riguardare l'andamento mensile e sistemare le lacune.
+//! NON DEPRIMERSI 
+//prompt completo Chat 
+// Da ora in poi farò degli snack. Ti darà una consegna e tu dovrai analizzare la consegna, analizzare il mio codice, darmi una valutazione da 1 a 10 e dirmi i pro e i contro, poi il codice corretto
