78f24dee80aed34e95fa6354eb3b43ab53b63686
Tamburini-Christofer <christofer.tamburini92@gmail.com>
2026-02-23 10:19:27 +0100

    Snack #137 SnowFlakeGenerator(null)

---
+++ 5.Dicembre/137.SnowFlakeGenerator-25Dicembre.js
@@ -0,0 +1,52 @@
+//todo Consegna dell'esercizio
+// Data una stringa multi-riga che utilizza caratteri di nuova riga (\n) per rappresentare un'interruzione di riga, restituisci una nuova stringa in cui ogni riga viene specchiata orizzontalmente e collegata alla fine della riga originale.
+//todo
+//! Requisiti da rispettare
+// Specchia una riga invertendo tutti i suoi caratteri, inclusi gli spazi.
+// Ad esempio, dato "* \n *\n* ", che viene registrato nella console come:
+
+// * 
+// *
+// * 
+// Restituisci "* *\n ** \n* *", che viene registrato nella console come:
+
+// * *
+// ** 
+// * *
+// Prendere nota degli spazi vuoti nelle stringhe fornite e restituite. Assicurarsi di non eliminarne nessuno.
+
+//!
+
+//* Tempo di esecuzione:
+    // Tempo impiegato 
+//*
+
+//*Codice scritto senza utilizzo di ChatGpt
+    // Codice scritto senza l'utilizzo di nessuna Ia
+//*
+
+//* Codice scritto da ChatGpt
+    // Codice scritto con l'ia, alternativa al mio o di completamento. 
+    // Se il tempo di esecuzione è stato rispettato, questo diventa un metodo alternativo di scrittura
+//*
+
+//* Valutazione al codice proposta da ChatGpt
+    // Valutazione numerica per futura media
+//*
+
+//* Pro e contro del mio codice
+    // Pro e contro e dove migliorare
+//*
+//! Altre considerazioni e nota bene
+//todo Esecuzione degli esercizi
+//? Creazione del file contenente "Numero snack"."Nome snack"-"Giorno del mese".js
+//? Comprensione della consegna ed eventuale copia in google translate. 
+//? 2 minuti massimo per analizzare la consegna e fare un pensiero prelimianre. 
+//? Inizio timer impostato ad un massimale di 30 minuti. Oltre questa soglia, l'esercizio non sarà completato
+//? Consultazione con ChatGpt per miglioramenti e sistemazioni
+//? Push finale
+//! Tre volte a settimana, riguardare lo snack e rivedere possibili soluzioni.
+//! Una volta a settimana, riguardare l'andamento mensile e sistemare le lacune.
+//! NON DEPRIMERSI 
+//prompt completo Chat 
+// Da ora in poi farò degli snack. Ti darà una consegna e tu dovrai analizzare la consegna, analizzare il mio codice, darmi una valutazione da 1 a 10 e dirmi i pro e i contro, poi il codice corretto
